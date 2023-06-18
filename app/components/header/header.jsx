a0aca9ce288341537d0454ef777eb2716a8c216f
Joseph Joy <joseph.joy@slashrtc.com>
2023-06-18 15:30:50 +0530

    added css in header

--- app/components/header/header.jsx
+++ app/components/header/header.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 
 import './header.scss';
+import logo from '../../images/cake-icon.jpg';
 
 export default function Header() {
   return (
-    <div className='header'>
+    <div className='header header__image'>
+      <div className="header__icon">
+        <img src={logo} alt="cake-icon" className='header__icon--img'/>
+      </div>
 
+      <div className="header__title--1">
+        <div className="header__title">
+          The
+        </div>
+
+        <div className="header__title">
+          Cake Shop
+        </div>
+      </div>
     </div>
   )
 }
